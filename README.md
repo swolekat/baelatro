28f158a2137402fd26efa9951a44f50bca07f63f
swolekat <swolecattwitch@gmail.com>
2025-10-15 13:38:36 -0500

    first cut

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# baelatro
+# Baelatro
 a swolebae themed balatro mod
+
+## Installation
+This requires you to use SMOD (installation instructions [here](https://github.com/Steamodded/smods/wiki)).
+
+To get the latest version of the mod check out the releases page on github.
+
+
+## Credit
+
+Lots of people worked on this project for simplicity's sake I'll divide this up into art credits and mod credits.
+
+### Art Credits
+* [EmergentComics](https://emergentcomics.com/) for making the vast majority of the models.
+* Witch_Hunter_ and stjimmy1995 for posing the models
+* [AstrowellTV](https://www.twitch.tv/astrowelltv) for the splash screen
+
+### Mod Credits
+* Swolekat (that's me) 
---
+++ baeltro.lua
@@ -0,0 +1,32 @@
+--- STEAMODDED HEADER
+--- MOD_NAME: Baelatro
+--- MOD_ID: Baelatro
+--- MOD_AUTHOR: [Swolekat, Witch_Hunter_, & Stjimmy1995]
+--- MOD_DESCRIPTION: Deck change
+
+----------------------------------------------
+------------MOD CODE -------------------------
+
+function SMODS.INIT.Baelatro()
+
+    local dec_mod = SMODS.findModByID("Baelatro")
+    --local sprite_card = SMODS.Sprite:new("cards_1", dec_mod.path, "IGT.png", 71, 95, "asset_atli")
+    --sprite_card:register()
+    --local sprite_card = SMODS.Sprite:new("cards_2", dec_mod.path, "IGTHC.png", 71, 95, "asset_atli")
+    --sprite_card:register()
+
+    --local sprite_jkr = SMODS.Sprite:new("Joker", dec_mod.path, "Jokers.png", 71, 95, "asset_atli")
+    --sprite_jkr:register()
+
+    local sprite_trt = SMODS.Sprite:new("Tarot", dec_mod.path, "baeTarots.png", 71, 95, "asset_atli")
+    sprite_trt:register()
+
+    local sprite_title = SMODS.Sprite:new("balatro", dec_mod.path, "test.jpg", 960, 540, "asset_atli")
+    sprite_title:register()
+
+    --SMODS.handle_loc_file(dec_mod.path .. 'text/')
+
+end
+
+----------------------------------------------
+------------MOD CODE END----------------------
